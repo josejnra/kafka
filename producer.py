0f8f66a7264a14c67c3976f5c386a708741ca4b3
José Nunes <josenunesra@gmail.com>
2021-04-28 22:35:27 -0300

    update producer message generator

--- producer.py
+++ producer.py
@@ -4,12 +4,10 @@ from uuid import uuid1
 
 from confluent_kafka import Producer
 
-topic_name = "users"
-producer = Producer({'bootstrap.servers': 'localhost:9092,localhost:9192'})
-size = 1000000
+producer = Producer({'bootstrap.servers': 'localhost:29092'})
 
 
-def get_message(size: int) -> bytes:
+def user_messsage(size: int) -> bytes:
     for _ in range(size):
         yield json.dumps({
             "nome": random.choice(["john", "alex", "jack", "whindersson"]),
@@ -17,16 +15,38 @@ def get_message(size: int) -> bytes:
         }).encode('utf-8')
 
 
+def location_messsage(size: int) -> bytes:
+    for _ in range(size):
+        yield json.dumps({
+            "profileId": str(uuid1(random.randint(1, 10000))),
+            "latitude": random.uniform(-14, -20),
+            "longitude": random.uniform(-40, -50)
+        }).encode('utf-8')
+
+
+def get_message_generator(topic_name: str, size: int):
+    generator = {
+        "users": user_messsage(size),
+        "locations": location_messsage(size)
+    }.get(topic_name)
+
+    if not generator:
+        raise KeyError('There is no such message generator:', topic_name)
+
+    return generator
+
+
 def delivery_report(err, decoded_message, original_message):
     if err is not None:
         print(err)
 
 
-def confluent_producer_async():
-    for msg in get_message(size):
+def confluent_producer_async(topic_name: str, size: int = 1000000):
+    for msg in get_message_generator(topic_name, size):
         producer.produce(
-            topic_name,
-            msg,
+            topic=topic_name,
+            key=str(uuid1(random.randint(1, 10000))),
+            value=msg,
             callback=lambda err, decoded_message, original_message=msg: delivery_report(  # noqa
                 err, decoded_message, original_message
             ),
@@ -34,12 +54,12 @@ def confluent_producer_async():
     producer.flush()
 
 
-def confluent_producer_sync():
-    for msg in get_message(size):
+def confluent_producer_sync(topic_name: str, size: int = 1000000):
+    for msg in get_message_generator(topic_name, size):
         producer.produce(
-            topic_name,
-            msg,
+            topic=topic_name,
             key=str(uuid1(random.randint(1, 10000))),
+            value=msg,
             callback=lambda err, decoded_message, original_message=msg: delivery_report(  # noqa
                 err, decoded_message, original_message
             ),
@@ -48,4 +68,4 @@ def confluent_producer_sync():
 
 
 if __name__ == '__main__':
-    confluent_producer_sync()
+    confluent_producer_sync('locations', 10000)
